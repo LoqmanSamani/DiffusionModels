b345e55521d1ca4eb57ec1be50387512d45750ef
LoqmanSamani <samaniloqman91@gmail.com>
2025-03-25 21:31:02 +0100

    working on the compression model paper

---
+++ ldm/network.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+
+class PerceptualAutoencoder(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+    def encoder(self, x):
+        return x
+
+    def decoder(self, x):
+        return x
+    
