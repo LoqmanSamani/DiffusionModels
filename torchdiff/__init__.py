f798229e32289492cf6312111e989e66722ae2f3
LoqmanSamani <samaniloqman91@gmail.com>
2025-08-22 09:51:39 +0200

    test the library

--- torchdiff/__init__.py
+++ torchdiff/__init__.py
@@ -1,8 +1,9 @@
-__version__ = "2.0.0"
+#__version__ = "2.0.0"
 
-from .ddim import ForwardDDIM, ReverseDDIM, VarianceSchedulerDDIM, TrainDDIM, SampleDDIM
-from .ddpm import ForwardDDPM, ReverseDDPM,  HyperParamsDDPM, TrainDDPM, SampleDDPM
-from .ldm import TrainLDM, TrainAE, AutoencoderLDM, SampleLDM
-from .sde import ForwardSDE, ReverseSDE, VarianceSchedulerSDE, TrainSDE, SampleSDE
+#from .ddim import ForwardDDIM, ReverseDDIM, VarianceSchedulerDDIM, TrainDDIM, SampleDDIM
+#from .ddpm import ForwardDDPM, ReverseDDPM,  VarianceSchedulerDDPM, TrainDDPM, SampleDDPM
+#from .ldm import TrainLDM, TrainAE, AutoencoderLDM, SampleLDM
+#from .sde import ForwardSDE, ReverseSDE, VarianceSchedulerSDE, TrainSDE, SampleSDE
 #from .unclip import ForwardCLIP, ReverseCLIP, HyperParamsCLIP, TrainCLIP, SampleCLIP
-from .utils import NoisePredictor, TextEncoder, Metrics
+#from .utils import NoisePredictor, TextEncoder, Metrics
+#from .test_ddpm import *
