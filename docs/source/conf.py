bace7e368e6dfde3c01b495702792950738a849b
LoqmanSamani <samaniloqman91@gmail.com>
2025-08-26 01:34:43 +0200

    Update docs for TorchDiff v2.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,20 +1,13 @@
-#import os
-#import sys
-#sys.path.insert(0, os.path.abspath('/home/loqman/Downloads/projs/TorchDiff'))  # Points to TorchDiff directory
 import os
 import sys
-from importlib.metadata import version as pkg_version
-
-
-
-sys.path.insert(0, os.path.abspath('../..'))  # Points to TorchDiff from docs/source/
-
+sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'TorchDiff'
-copyright = '2025, Loghman Samani'
 author = 'Loghman Samani'
-release = pkg_version("torchdiff")  # reads __version__ from installed package
-version = release
+
+# Optionally auto-read version from package to avoid manual bumps
+from importlib.metadata import version as pkg_version
+release = version = pkg_version("torchdiff")
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -28,10 +21,14 @@ exclude_patterns = []
 html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 
-# Autodoc settings
 autoclass_content = 'both'
 autodoc_member_order = 'bysource'
 
-autodoc_mock_imports = ["torch", "torchvision", "torchaudio", "lpips", "pytorch_fid", "transformers"]
+# Mock heavy imports so RTD build doesn’t break
+autodoc_mock_imports = [
+    "torch", "torchvision", "torchaudio",
+    "lpips", "pytorch_fid", "transformers"
+]
+
 
 
